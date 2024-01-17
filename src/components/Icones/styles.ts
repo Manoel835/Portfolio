a047a995046c8acc4cfe3fdbed567fd46205b347
Manoel835 <mftneto@gmail.com>
2024-01-16 23:21:40 -0300

    Ajuste

--- src/components/Icones/styles.ts
+++ src/components/Icones/styles.ts
@@ -11,7 +11,7 @@ export const Container = styled.div`
     width: 100vw;
     flex-direction: row;
     align-items: center;
-    margin-top: 130px;
+    margin-top: 200px;
   }
 `
 export const Line = styled.div`
