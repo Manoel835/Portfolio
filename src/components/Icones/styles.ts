611167552089371367284e7d2a02e559074202fa
Manoel835 <mftneto@gmail.com>
2024-01-16 23:08:02 -0300

    Icones

--- src/components/Icones/styles.ts
+++ src/components/Icones/styles.ts
@@ -11,7 +11,7 @@ export const Container = styled.div`
     width: 100vw;
     flex-direction: row;
     align-items: center;
-    margin-top: 110px;
+    margin-top: 130px;
   }
 `
 export const Line = styled.div`
